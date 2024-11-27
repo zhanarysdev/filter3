e22e1174e103652704c7556911666e0738e827d8
zhanarysdev <zhanarysdev@gmail.com>
2024-11-27 14:57:51 +0600

    update

--- components/overlay.tsx
+++ components/overlay.tsx
@@ -1,4 +1,4 @@
-'use clientj'
+'use client'
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Explore } from "./explore";
@@ -7,7 +7,7 @@ export function Overlay() {
     const pathname = usePathname()
     return (
 
-        <div className="bg-transparent flex flex-col justify-end  fixed  left-0 right-[50%] bottom-0 p-[16px] md:p-[20px]">
+        <div className="hidden bg-transparent xl:flex flex-col justify-end  fixed  left-0 right-[50%] bottom-0 p-[16px] md:p-[20px]">
             <div className="text-[#929292] text-[14px] leading-[18px] md:leading-[20px] flex justify-between items-end xl:text-[18px] xl:leading-[24px]">
                 <div className="flex flex-col gap-[8px]">
                     <Link className={`hover:text-primary xl:hidden`} style={{ color: pathname === "/form" ? "#373737" : "" }} href="/form">Help Center</Link>
