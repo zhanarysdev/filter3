6a7e4b2d3aaaba1e80077edaf032055d63dff356
zhanarysdev <zhanarysdev@gmail.com>
2024-11-28 12:34:29 +0600

    added new select component

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,99 +1,133 @@
-'use client'
-import { PageWrapper } from "@/components/page-wrapper";
-import { Text } from "@/components/text";
-import { Title } from "@/components/title";
-import { useForm } from "react-hook-form"
-import { yupResolver } from "@hookform/resolvers/yup"
-import * as yup from "yup"
-import Select from "@/components/select";
+"use client";
 import { Input } from "@/components/input";
+import { PageWrapper } from "@/components/page-wrapper";
+import { Select } from "@/components/select";
+import { Text } from "@/components/text";
 import { Textarea } from "@/components/textarea";
+import { Title } from "@/components/title";
 import UploadInput from "@/components/upload-input";
-import BigForm from "@/components/big-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
 
 interface IForm {
-    email: string
-    type: string
+  email: string;
+  type: string;
 }
-const schema = yup.object({
+const schema = yup
+  .object({
     email: yup.string().email().required(),
-    type: yup.string().required()
-}).required()
+    type: yup.string().required(),
+  })
+  .required();
 
 export default function Form() {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm<IForm>({
-        resolver: yupResolver(schema),
-    })
-    const onSubmit = (data: IForm) => console.log(data)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>({
+    resolver: yupResolver(schema),
+  });
+  const onSubmit = (data: IForm) => console.log(data);
 
-    return (
-        <>
-            <div className="xl:hidden">
-                <PageWrapper>
-                    <Title data={'Fill the form'} />
-                    <div className="mt-[8px]">
-                        <Text data="This may mean our list of supported countries and regions could differ from PayPal’s official list. While we strive to keep this list current, the supported locations may occasionally be updated by PayPal/Hyperwallet. For the most up-to-date information, please refer to PayPal support." />
-                    </div>
-                    <form onSubmit={handleSubmit(onSubmit)} className="mt-[32px] flex flex-col gap-[16px] pb-[16px]">
-                        <div className="flex flex-col">
-                            <label htmlFor="type" className="text-[14px] leading-[18px] font-bold mb-[8px]">
-                                Please choose a request type below
-                            </label>
-                            <Select options={[{ value: 0, label: "I am a creator and I need support" }, { value: 1, label: 'test' }]} {...register("type")} />
-                        </div>
-
-                        <div className="flex flex-col">
-                            <label htmlFor="email" className="text-[14px] leading-[18px] font-bold mb-[8px]">
-                                Your email address*
-                            </label>
-                            <Input placeholder="Email" id="email" type="text" />
-                        </div>
-
-                        <div className="flex flex-col">
-                            <label htmlFor="type" className="text-[14px] leading-[18px] font-bold mb-[8px]">
-                                Please select one of the options below*
-                            </label>
-                            <Select options={[{ value: 0, label: "-" }, { value: 1, label: 'test' }]} {...register("type")} />
-                        </div>
-
-                        <div className="flex flex-col">
-                            <label htmlFor="email" className="text-[14px] leading-[18px] font-bold mb-[8px]">
-                                Subject*
-                            </label>
-                            <Input placeholder="Email" id="email" type="text" />
-                            <span className="text-[#929292] text-[14px] leading-[18px] mt-[8px]">Please use a few words to summarize your question</span>
-                        </div>
-
-                        <div className="flex flex-col">
-                            <label htmlFor="email" className="text-[14px] leading-[18px] font-bold mb-[8px]">
-                                Description*
-                            </label>
-                            <Textarea placeholder="Email" id="email" />
-                            <span className="text-[#929292] text-[14px] leading-[18px] mt-[8px]">
-                                Please enter the details of your request. A member
-                                of our team will reach out with a response.</span>
-                        </div>
-
-                        <div className="flex flex-col">
-                            <label htmlFor="type" className="text-[14px] leading-[18px] font-bold mb-[8px]">
-                                Attachments(optional)
-                            </label>
-                            <UploadInput />
-                        </div>
-
-                        <button className="bg-[#929292] text-[#F0F0F0] text-[14px] font-bold leading-[18px] p-[16px] rounded-[8px]">
-                            Submit
-                        </button>
-
-                    </form>
-                </PageWrapper>
+  return (
+    <>
+      <div className="xl:hidden">
+        <PageWrapper>
+          <Title data={"Fill the form"} />
+          <div className="mt-[8px]">
+            <Text data="This may mean our list of supported countries and regions could differ from PayPal’s official list. While we strive to keep this list current, the supported locations may occasionally be updated by PayPal/Hyperwallet. For the most up-to-date information, please refer to PayPal support." />
+          </div>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="mt-[32px] flex flex-col gap-[16px] pb-[16px]"
+          >
+            <div className="flex flex-col">
+              <label
+                htmlFor="type"
+                className="text-[14px] leading-[18px] font-bold mb-[8px]"
+              >
+                Please choose a request type below
+              </label>
+              <Select
+                options={[
+                  { value: 0, label: "I am a creator and I need support" },
+                  { value: 1, label: "test" },
+                  { value: 2, label: "test" },
+                ]}
+                {...register("type")}
+              />
             </div>
 
-            <BigForm />
-        </>
-    )
+            <div className="flex flex-col">
+              <label
+                htmlFor="email"
+                className="text-[14px] leading-[18px] font-bold mb-[8px]"
+              >
+                Your email address*
+              </label>
+              <Input placeholder="Email" id="email" type="text" />
+            </div>
+
+            <div className="flex flex-col">
+              <label
+                htmlFor="type"
+                className="text-[14px] leading-[18px] font-bold mb-[8px]"
+              >
+                Please select one of the options below*
+              </label>
+              <Select
+                options={[
+                  { value: 0, label: "-" },
+                  { value: 1, label: "test" },
+                ]}
+              />
+            </div>
+
+            <div className="flex flex-col">
+              <label
+                htmlFor="email"
+                className="text-[14px] leading-[18px] font-bold mb-[8px]"
+              >
+                Subject*
+              </label>
+              <Input placeholder="Email" id="email" type="text" />
+              <span className="text-[#929292] text-[14px] leading-[18px] mt-[8px]">
+                Please use a few words to summarize your question
+              </span>
+            </div>
+
+            <div className="flex flex-col">
+              <label
+                htmlFor="email"
+                className="text-[14px] leading-[18px] font-bold mb-[8px]"
+              >
+                Description*
+              </label>
+              <Textarea placeholder="Email" id="email" />
+              <span className="text-[#929292] text-[14px] leading-[18px] mt-[8px]">
+                Please enter the details of your request. A member of our team
+                will reach out with a response.
+              </span>
+            </div>
+
+            <div className="flex flex-col">
+              <label
+                htmlFor="type"
+                className="text-[14px] leading-[18px] font-bold mb-[8px]"
+              >
+                Attachments(optional)
+              </label>
+              <UploadInput />
+            </div>
+
+            <button className="bg-[#929292] text-[#F0F0F0] text-[14px] font-bold leading-[18px] p-[16px] rounded-[8px]">
+              Submit
+            </button>
+          </form>
+        </PageWrapper>
+      </div>
+    </>
+  );
 }
--- components/select.tsx
+++ components/select.tsx
@@ -1,34 +1,45 @@
-'use client'
-import { ComponentProps, forwardRef } from "react";
+"use client";
+import { ComponentProps, forwardRef, useState } from "react";
+import { Icon } from "./icons";
 
 export type SelectOptionProps = { value: number; label: string }[];
 
 export type SelectProps = {
-    options: SelectOptionProps;
-    placeholder?: string;
-    name?: string;
+  options: SelectOptionProps;
+  placeholder?: string;
+  name?: string;
 } & ComponentProps<"select">;
 
-const Select = forwardRef<HTMLSelectElement, SelectProps>(
-    ({ options, placeholder, onChange, onBlur, name }, ref) => {
-        return (
-            <select
-                name={name}
-                ref={ref}
-                onChange={onChange}
-                onBlur={onBlur}
-                className={"rounded-[8px] bg-[#FFFFFF] p-[16px] w-full"}
+export const Select = ({ options }: SelectProps) => {
+  const [isOpen, setOpen] = useState(false);
+  const [selected, setSelected] = useState(options[0]);
+  return (
+    <div className="transition-all rounded-[8px] flex flex-col bg-[#FFFFFF] p-[16px] w-full">
+      <div
+        className="flex cursor-pointer w-full items-center"
+        onClick={() => setOpen((old) => !old)}
+      >
+        <div className="flex-1">{selected.label}</div>
+        <div>
+          <Icon name="CaretDown" />
+        </div>
+      </div>
+      {isOpen && (
+        <div className="flex flex-col gap-[8px] mt-[8px]">
+          {options.map((el) => (
+            <div
+              className="text-[#929292] text-[14px] leading-[18px] font-medium hover:text-primary cursor-pointer"
+              key={el.value}
+              onClick={() => {
+                setSelected(el);
+                setOpen(false);
+              }}
             >
-                {placeholder && <option value="">{placeholder}</option>}
-                {options.map(({ value, label }) => (
-                    <option key={value} value={value}>
-                        {label}
-                    </option>
-                ))}
-            </select>
-        );
-    },
-);
-
-Select.displayName = "Select"
-export default Select
+              {el.label}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
